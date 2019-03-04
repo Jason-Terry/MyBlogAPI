7505632497cdcdce7ee4953962fdbe554db73467
JasonTerry <jasonalanterry@gmail.com>
2019-03-04 02:06:01 -0500

    Add express, add body-parser, set up test route.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("API SERVER RUNNING!");
+// lib/app.ts
+import express = require('express');
+
+// Create a new express application instance
+const app: express.Application = express();
+const port: number = 1313
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port ' + port + ".");
+});
