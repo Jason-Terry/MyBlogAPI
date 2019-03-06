656833de1ed53fe6745ff6da77440fdc531effe6
JasonTerry <jasonalanterry@gmail.com>
2019-03-06 12:54:26 -0500

    Add new routes for handling logins, and new users.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,7 @@
 // api.ts
 import express = require('express');
 import mongoose = require('mongoose');
-import { User } from '../schemas/user'
+import { User, UserModel } from '../schemas/user'
 
 // DB CONNECTION
 const URI = "mongodb+srv://User:IrDxlO8uO4T6sPzo@cluster0-rpvs1.azure.mongodb.net/BlogDB?retryWrites=true";
@@ -14,25 +14,46 @@ mongoose.connect(URI, (err) => {
 });
 // END DB CONNECTION
 
-
-
+// BEGIN API ROUTES
 const ROUTER = express.Router();
 
 ROUTER.get('/', (req, res) => {
     res.send('From API route.')
-})
+});
 
 ROUTER.post('/register', (req, res) => {
     let uData = req.body;
     const UserModel = new User().getModelForClass(User);
     const u = new UserModel(uData);
-    u.save((err)=> {
+    u.save((err, user)=> {
         if (err) {
             console.error(err);
         } else {
+            res.status(200).send(user)
             console.log("Save Success");
         }
     });
-})
+});
+
+ROUTER.post('/login', (req, res) => {
+    let uData = req.body;
+    UserModel.findOne({email: uData.email}, (err, user) => {
+        if (err) {
+            console.error(err);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid Email');
+            } else {
+                if (user.password != uData.password) {
+                    res.status(401).send('Invalid Password');
+                } else {
+                    res.status(200).send(user);
+                }
+            }
+        }
+    });
+});
+
+ROUTER.post('/get-blog')
 
 module.exports = ROUTER;
--- src/schemas/post.ts
+++ src/schemas/post.ts
@@ -1,12 +1,31 @@
-import mongoose = require('mongoose');
+import * as mongoose from 'mongoose';
+import { prop, Typegoose } from 'typegoose';
 
-const postSchema: mongoose.Schema = new mongoose.Schema({
-    title: String,
-    author: String,
-    dateCreated: String,
-    dateEdited: String,
-    body: String
-});
+export class Article extends Typegoose{
+    // properties
+    // |
+    // v
+    @prop()
+    title?: String;
 
-// model name, schema, and collection.
-module.exports = mongoose.model('post', postSchema, 'BlogHist');
+    @prop()
+    author?: String;
+
+    @prop()
+    dateCreated?: String;
+
+    @prop()
+    dateEdited?: String;
+
+    @prop()
+    body?: String;
+}
+
+export const UserModel = new Article().getModelForClass(Article, {
+    existingMongoose: mongoose,
+    //  had many problems without that definition of the collection name
+    //  so better define it
+    //                        |
+    //                        v
+    schemaOptions: {collection: 'BlogHist'}
+})
